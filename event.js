897ce226ad6243afd636f9c18732d0eac8ce2b68
Better Mistakes <123073428+bettermistakes@users.noreply.github.com>
2026-02-26 10:47:16 +0000

    Create event.js

---
+++ event.js
@@ -0,0 +1,28 @@
+(() => {
+  try {
+    const el = document.querySelector(".splide.is-team");
+    if (!window.Splide || !el) return;
+
+    new Splide(el, {
+      type: "loop",
+      perPage: 3,
+      perMove: 1,
+      focus: "left",
+      speed: 500,
+      drag: false,
+      gap: "4rem",
+      autoplay: false,
+      interval: 2000,
+      arrows: true,
+      pauseOnHover: true,
+      keyboard: true,
+      rewind: false,
+      pagination: true,
+      reducedMotion: { speed: 0, rewindSpeed: 0 },
+      breakpoints: {
+        991: { perPage: 1, gap: "1rem" },
+        767: { perPage: 1, gap: "1rem" },
+      },
+    }).mount();
+  } catch (e) {}
+})();
